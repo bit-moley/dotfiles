730a5d786465e3ad64e592e84c8bcbd4f1a8dc28
Joel Felton <joelfelton@pm.me>
2026-03-21 09:19:42 +0000

    something close to a final setup with hyprland

---
+++ thinkpad/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+require("config.lazy")
+require("config.keymaps")
+require("config.options")
+require("config.colour")
+
+--- LSP CONFIG ---
+vim.lsp.enable({ "lua_ls", "rust_analyzer", "basedpyright", "gopls", "clangd", "ols", "zls"})
+vim.lsp.set_log_level("ERROR")
